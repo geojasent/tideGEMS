225e680dadb42ae1910b90b17b212ac6bc8a87d8
Jason Wong <wongjason415@gmail.com>
2023-04-26 23:01:38 -0700

    seed NoaaSixMinTideByDate

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Seed using rest-client
+gem "rest-client"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'rest-client'
+
+puts 'Getting tide data'
+def tides_from_noaa
+  begin_date = '20230501'
+  end_date = '20230507'
+  station_number = '9414290'
+  response = RestClient.get("https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=#{begin_date}&end_date=#{end_date}&station=#{station_number}&product=predictions&datum=NAVD&time_zone=lst&units=english&format=json")
+  parsed_json = JSON.parse(response.body)
+
+  prev_date_time = Time.parse begin_date
+  prev_date = prev_date_time.strftime('%Y-%m-%d')
+  predictions = []
+
+  parsed_json['predictions'].each do |data|
+    date_time = Time.parse data['t']
+    date = date_time.strftime('%Y-%m-%d')
+
+    if date == prev_date
+      predictions << [data['t'], data['v']]
+    else
+      NoaaSixMinTideByDate.create(
+        station_number: station_number,
+        date: date,
+        data: predictions
+      )
+      predictions = []
+      prev_date = date
+    end
+  end
+end
+
+tides_from_noaa()
+puts 'Seeding tide data'
