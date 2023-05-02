c9850ccd0b34eff52d286b5701a0af9210e8b946
Jason Wong <wongjason415@gmail.com>
2023-05-02 16:35:12 -0700

    add homepage chart date selection

--- app/controllers/tides_controller.rb
+++ app/controllers/tides_controller.rb
@@ -5,9 +5,12 @@ class TidesController < ApplicationController
     db_date = converted_date.strftime('%Y-%m-%d')
     end_date = '20230507'
     tide_station = '9414290'
+    current_date = DateTime.now.strftime('%Y-%m-%d')
+    @selected_date = params[:date] || current_date
 
-    tide_data = NoaaSixMinTideByDate.all
-    @daily_data = JSON.parse tide_data[0].data
+    @tide_data = NoaaSixMinTideByDate.all
+    @daily_data = @tide_data.where(date: @selected_date)
+    @daily_data_date = @tide_data.pluck(:date)
   end
 
   def weekly_tide
