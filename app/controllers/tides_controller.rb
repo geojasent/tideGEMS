bf839706ae0a65e35b4b44e9bf3dba0952d0e3ce
Jason Wong <wongjason415@gmail.com>
2023-05-08 16:02:41 -0700

    add sunrise sunset to chart

--- app/controllers/tides_controller.rb
+++ app/controllers/tides_controller.rb
@@ -8,14 +8,36 @@ class TidesController < ApplicationController
     current_date = DateTime.now.strftime('%Y-%m-%d')
     @selected_date = params[:date] || current_date
 
-    @tide_data = NoaaSixMinTideByDate.all
-    @daily_data = @tide_data.where(date: @selected_date)
-    @daily_data_date = @tide_data.pluck(:date)
+    tide_data = NoaaSixMinTideByDate.all
+    @daily_data = tide_data.where(date: @selected_date)
+    @daily_data_date = tide_data.pluck(:date)  
     @chart_daily_data = JSON.parse @daily_data[0].data
 
+    sunrise_sunset_data = SunriseSunset.all
+    daily_sunrise_sunset_data = sunrise_sunset_data.where(date: @selected_date)
+    sunrise = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunrise].join(' '))
+    sunrise_string = sunrise.strftime('%Y-%m-%d %H:%M')
+    # @sunrise = daily_sunrise_sunset_data[0].sunrise
+    sunset = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunset].join(' '))
+    # @sunset = daily_sunrise_sunset_data[0].sunset
+    sunset_string = sunset.strftime('%Y-%m-%d %H:%M')
+
+    # combine tide and sunrise sunset data
+    @tide_sun_arr = []
+    tide_hash = {}
+    sunrise_hash = {}
+    sunset_hash = {}
+    tide_hash['name'] = 'Tide'
+    tide_hash['data'] = @chart_daily_data.to_h
+    @tide_sun_arr << tide_hash
+    sunrise_hash['name'] = 'Sunrise'
+    sunrise_hash['data'] = {sunrise_string => 0}
+    @tide_sun_arr << sunrise_hash
+    sunset_hash['name'] = 'Sunset'
+    sunset_hash['data'] = {sunset_string => 0}
+    @tide_sun_arr << sunset_hash
   end
 
   def weekly_tide
   end
-  
 end
