dece10891e8d249b54d7a092b29b023be0f6b977
Jason Wong <wongjason415@gmail.com>
2023-05-09 14:10:54 -0700

    fix weekly date selection

--- app/controllers/tides_controller.rb
+++ app/controllers/tides_controller.rb
@@ -10,16 +10,14 @@ class TidesController < ApplicationController
 
     tide_data = NoaaSixMinTideByDate.all
     @daily_data = tide_data.where(date: @selected_date)
-    @daily_data_date = tide_data.pluck(:date)  
+    @daily_data_date = tide_data.pluck(:date)
     @chart_daily_data = JSON.parse @daily_data[0].data
 
     sunrise_sunset_data = SunriseSunset.all
     daily_sunrise_sunset_data = sunrise_sunset_data.where(date: @selected_date)
     sunrise = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunrise].join(' '))
     sunrise_string = sunrise.strftime('%Y-%m-%d %H:%M')
-    # @sunrise = daily_sunrise_sunset_data[0].sunrise
     sunset = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunset].join(' '))
-    # @sunset = daily_sunrise_sunset_data[0].sunset
     sunset_string = sunset.strftime('%Y-%m-%d %H:%M')
 
     # combine tide and sunrise sunset data
@@ -38,6 +36,48 @@ class TidesController < ApplicationController
     @tide_sun_arr << sunset_hash
   end
 
-  def weekly_tide
+  def weekly_view
+    begin_date = '20230501'
+    converted_date = Time.parse begin_date
+    db_date = converted_date.strftime('%Y-%m-%d')
+    end_date = '20230507'
+    tide_station = '9414290'
+    sunday_date = Date.today.prev_occurring(:sunday).to_s
+    @selected_date = params[:date] || sunday_date
+    end_date = (@selected_date.to_date + 7).to_s
+
+    # get tide data
+    tide_data = NoaaSixMinTideByDate.all
+    @daily_data = tide_data.where(date: @selected_date..end_date)
+    # get sundays
+    my_day = [0]
+    @daily_data_date = tide_data.pluck(:date).to_a.select { |k| my_day.include?(k.wday) }
+    @chart_weekly_data = []
+    for i in 0..6 do
+      temp = JSON.parse @daily_data[i].data
+      @chart_weekly_data << temp
+    end
+
+    sunrise_sunset_data = SunriseSunset.all
+    daily_sunrise_sunset_data = sunrise_sunset_data.where(date: @selected_date..end_date)
+    sunrise = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunrise].join(' '))
+    sunrise_string = sunrise.strftime('%Y-%m-%d %H:%M')
+    sunset = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunset].join(' '))
+    sunset_string = sunset.strftime('%Y-%m-%d %H:%M')
+
+    # combine tide and sunrise sunset data
+    @tide_sun_arr = []
+    tide_hash = {}
+    sunrise_hash = {}
+    sunset_hash = {}
+    # tide_hash['name'] = 'Tide'
+    # tide_hash['data'] = @chart_weekly_data.to_h
+    # @tide_sun_arr << tide_hash
+    # sunrise_hash['name'] = 'Sunrise'
+    # sunrise_hash['data'] = {sunrise_string => 0}
+    # @tide_sun_arr << sunrise_hash
+    # sunset_hash['name'] = 'Sunset'
+    # sunset_hash['data'] = {sunset_string => 0}
+    # @tide_sun_arr << sunset_hash
   end
 end
