190a6fa7c9d831a43675d89493aadd04d9a4f07e
Jason Wong <wongjason415@gmail.com>
2023-05-10 16:05:09 -0700

    add weekly tide chart

--- app/controllers/tides_controller.rb
+++ app/controllers/tides_controller.rb
@@ -29,10 +29,10 @@ class TidesController < ApplicationController
     tide_hash['data'] = @chart_daily_data.to_h
     @tide_sun_arr << tide_hash
     sunrise_hash['name'] = 'Sunrise'
-    sunrise_hash['data'] = {sunrise_string => 0}
+    sunrise_hash['data'] = { sunrise_string => 0 }
     @tide_sun_arr << sunrise_hash
     sunset_hash['name'] = 'Sunset'
-    sunset_hash['data'] = {sunset_string => 0}
+    sunset_hash['data'] = { sunset_string => 0 }
     @tide_sun_arr << sunset_hash
   end
 
@@ -52,32 +52,44 @@ class TidesController < ApplicationController
     # get sundays
     my_day = [0]
     @daily_data_date = tide_data.pluck(:date).to_a.select { |k| my_day.include?(k.wday) }
+
     @chart_weekly_data = []
     for i in 0..6 do
       temp = JSON.parse @daily_data[i].data
       @chart_weekly_data << temp
     end
+    @chart_weekly_data = @chart_weekly_data.flatten(1)
 
+    # get sunset data
+    sun_array = []
+    sunrise_array = []
+    sunset_array = []
     sunrise_sunset_data = SunriseSunset.all
     daily_sunrise_sunset_data = sunrise_sunset_data.where(date: @selected_date..end_date)
-    sunrise = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunrise].join(' '))
-    sunrise_string = sunrise.strftime('%Y-%m-%d %H:%M')
-    sunset = DateTime.parse([@selected_date, daily_sunrise_sunset_data[0].sunset].join(' '))
-    sunset_string = sunset.strftime('%Y-%m-%d %H:%M')
+    for i in 0..6 do
+      sunrise = DateTime.parse([(@selected_date.to_date + i).to_s, daily_sunrise_sunset_data[i].sunrise].join(' '))
+      sunrise_string = sunrise.strftime('%Y-%m-%d %H:%M')
+      sun_array << sunrise_string
+      sunrise_array << [sunrise_string, 0]
+      sunset = DateTime.parse([(@selected_date.to_date + i).to_s, daily_sunrise_sunset_data[i].sunset].join(' '))
+      sunset_string = sunset.strftime('%Y-%m-%d %H:%M')
+      sun_array << sunset_string
+      sunset_array << [sunset_string, 0]
+    end
 
     # combine tide and sunrise sunset data
-    @tide_sun_arr = []
+    @week_tide_sun_array = []
     tide_hash = {}
     sunrise_hash = {}
     sunset_hash = {}
-    # tide_hash['name'] = 'Tide'
-    # tide_hash['data'] = @chart_weekly_data.to_h
-    # @tide_sun_arr << tide_hash
-    # sunrise_hash['name'] = 'Sunrise'
-    # sunrise_hash['data'] = {sunrise_string => 0}
-    # @tide_sun_arr << sunrise_hash
-    # sunset_hash['name'] = 'Sunset'
-    # sunset_hash['data'] = {sunset_string => 0}
-    # @tide_sun_arr << sunset_hash
+    tide_hash['name'] = 'Tide'
+    sunrise_hash['name'] = 'Sunrise'
+    sunset_hash['name'] = 'Sunset'
+    tide_hash['data'] = @chart_weekly_data.to_h
+    sunrise_hash['data'] = sunrise_array.to_h
+    sunset_hash['data'] = sunset_array.to_h
+    @week_tide_sun_array << tide_hash
+    @week_tide_sun_array << sunrise_hash
+    @week_tide_sun_array << sunset_hash
   end
 end
